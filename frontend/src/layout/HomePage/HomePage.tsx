b439503fb05c3a3a012152c97f27d35fe21e5bf4
lewisc99 <lewiscontato99@gmail.com>
2026-02-07 10:09:20 -0300

    feat: add Heros and LibraryServices components, enhance Carousel layout

--- frontend/src/layout/HomePage/HomePage.tsx
+++ frontend/src/layout/HomePage/HomePage.tsx
@@ -1,11 +1,15 @@
 import { Carousel } from "./Components/Carousel"
 import { ExploreTopBooks } from "./Components/ExporeTopBooks"
+import { Heros } from "./Components/Heros"
+import { LibraryServices } from "./Components/LibraryService"
 
 export const HomePage = () => {
     return (
         <>
         <ExploreTopBooks />
         <Carousel />
+        <Heros/>
+        <LibraryServices />
         </>
     )
 }
